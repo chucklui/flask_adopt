920a58885636a6a330a93476f2c13c5b8110146a
Chuck Lui <luialex0@gmail.com>
2022-02-04 11:43:15 -0800

    started the homepage route and templates

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 """Flask app for adopt app."""
-
-from flask import Flask
+from models import Pet
+from flask import Flask, render_template
 
 from flask_debugtoolbar import DebugToolbarExtension
 
@@ -22,3 +22,9 @@ db.create_all()
 # app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 toolbar = DebugToolbarExtension(app)
+
+@app.get('/')
+def homepage():
+    """doc"""
+    pets = Pet.query.all()
+    return render_template('list_pets.html', pets = pets)
--- models.py
+++ models.py
@@ -13,3 +13,21 @@ def connect_db(app):
 
     db.app = app
     db.init_app(app)
+
+
+class Pet(db.Model):
+    """Pet."""
+
+    __tablename__ = "pets"
+
+    id = db.Column(db.Integer,
+                   primary_key=True,
+                   autoincrement=True)
+    name = db.Column(db.Text, nullable=False)
+    species = db.Column(db.Text, nullable=False)
+    photo_url = db.Column(db.Text,
+                          nullable=False,
+                          default='')
+    age = db.Column(db.Text, nullable=False)
+    notes = db.Column(db.Text, nullable=True)
+    available = db.Column(db.Boolean, default=True)
